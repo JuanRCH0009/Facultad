28cfce0f676e8f67722609db199e9475b601297c
Juan Cruz Reche <juancruzreche17@gmail.com>
2022-11-13 13:01:04 -0300

    q

---
+++ Java/Leccion 9/Arreglos/src/Lecc9Ejer1/Lecc9Ejer1.java
@@ -0,0 +1,20 @@
+package Lecc9Ejer1;
+
+import java.util.Scanner;
+
+public class Lecc9Ejer1 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese un numero");
+        int num = entrada.nextInt();
+        int nums[]=new int[5];
+        while (num >= 5) {
+            for (int i = 0; i < nums.length; i++) {
+                
+
+            }
+
+            
+        }
+    }
+}
